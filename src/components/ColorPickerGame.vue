6a8db12c4419f7e214eac9b20395b277c684518e
Naomi <naomiokorodudu@gmail.com>
2023-02-07 21:45:06 -0800

    convert to composables

---
+++ src/components/ColorPickerGame.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>Color Picker Game</h1>
+
+  <div>{{ message }}</div>
+
+  <button v-for="color in colors" :key="color" @click="matchColor(color)">
+    {{ color }}
+  </button>
+</template>
+
+<script>
+//import { ref, reactive } from "@vue/reactivity";
+import ColorPickerLogic from "../composables/ColorPickerLogic";
+
+export default {
+  setup() {
+   
+    const { colors, message, matchColor } = ColorPickerLogic();
+
+    return {
+      colors,
+      message,
+      matchColor,
+    };
+  },
+};
+</script>
+
+<style></style>
